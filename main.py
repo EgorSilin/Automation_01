48a73c027a9c976e86f165688b43fb205faeb82e
Egor <egorsilin@gmail.com>
2020-11-05 17:22:40 +0500

    added file reader

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pymysql
 
 
 def get_con_fb_db():
+    """Connect to firebird"""
     con_fb_db_int = fdb.connect(dsn='127.0.0.1:/var/lib/firebird/3.0/data/name_db.fdb',
                                 user='sysdba',
                                 password='user@7136498',
@@ -13,6 +14,7 @@ def get_con_fb_db():
 
 
 def get_con_mysql_db():
+    """Connect to mysql"""
     con_mysql_db_int = pymysql.connect(host="localhost",
                                        user="root",
                                        password="root@mysql",
@@ -21,28 +23,26 @@ def get_con_mysql_db():
 
 
 def write_to_file(path, sql_req):
-    # Запись в файл
+    """Take data from firebird table and put in file"""
     try:
         with open(path, 'w') as f:  # W - for test; X - for prod;
             con_fb_db = get_con_fb_db()
             try:
                 cur_fb_db = con_fb_db.cursor()
                 cur_fb_db.execute(sql_req)
-                ########
                 for fieldDesc in cur_fb_db.description:
                     # f.write(fieldDesc[fdb.DESCRIPTION_NAME].ljust(fieldDesc[fdb.DESCRIPTION_DISPLAY_SIZE]) + '\n')  # , end="|"
                     f.write(fieldDesc[fdb.DESCRIPTION_NAME] + '|')  # , end="|"
                 f.write('\n')
                 # вывод данных таблицы
-                fieldIndices = range(len(cur_fb_db.description))
+                field_indices = range(len(cur_fb_db.description))
                 for row in cur_fb_db:
-                    for fieldIndex in fieldIndices:
-                        fieldValue = str(row[fieldIndex])
-                        fieldMaxWidth = cur_fb_db.description[fieldIndex][fdb.DESCRIPTION_DISPLAY_SIZE]
+                    for fieldIndex in field_indices:
+                        field_value = str(row[fieldIndex])
+                        # fieldMaxWidth = cur_fb_db.description[fieldIndex][fdb.DESCRIPTION_DISPLAY_SIZE]
                         # f.write(fieldValue.ljust(fieldMaxWidth) + '|')  # , end="|"
-                        f.write(fieldValue + '|')  # , end="|"
+                        f.write(field_value + '|')  # , end="|"
                     f.write('\n')
-                ########
                 cur_fb_db.close()
             finally:
                 con_fb_db.close()
@@ -51,30 +51,39 @@ def write_to_file(path, sql_req):
 
 
 def read_file(path):
+    """Read text file to list of lists"""  # Оптимизировать на построчное чтение
     lst_of_str = []
     try:
         with open(path, 'r') as f:
             for line in f:
                 lst_of_str.append([line.split('|')[0], line.split('|')[1]])
-                print(line)
+                # print(line)  # for TS
     except IOError:
         print("An IOError has occurred!")
-    print(lst_of_str)
+    # print(lst_of_str)  # for TS
+    return lst_of_str
 
 
 if __name__ == '__main__':
-    if os.path.exists('/home/user/first_name.txt') and os.path.exists('/home/user/first_name.txt'):
-        sys.exit("Data file(-s) already exist!")
-    else:
-        write_to_file(path='/home/user/first_name.txt',
-                      sql_req="SELECT ID,NAME FROM FIRST_NAME")
-        write_to_file(path='/home/user/last_name.txt',
-                      sql_req="SELECT ID,NAME FROM LAST_NAME")
-    # data_files_path = 'C:\Users\EgorS\PycharmProjects\Automation_01\data_files'
-    # Чтение из файла
-    read_file(path='/home/user/first_name.txt')
-    read_file(path='/home/user/last_name.txt')
+    # Чтение в файл из firebird
+    write_to_file(path='/home/user/first_name.txt',
+                  sql_req="SELECT ID,NAME FROM FIRST_NAME")
+    write_to_file(path='/home/user/last_name.txt',
+                  sql_req="SELECT ID,NAME FROM LAST_NAME")
+    # if os.path.exists('/home/user/first_name.txt') and os.path.exists('/home/user/first_name.txt'):
+    #     sys.exit("Data file(-s) already exist!")
+    # else:
+    #     write_to_file(path='/home/user/first_name.txt',
+    #                   sql_req="SELECT ID,NAME FROM FIRST_NAME")
+    #     write_to_file(path='/home/user/last_name.txt',
+    #                   sql_req="SELECT ID,NAME FROM LAST_NAME")
 
+    # Чтение из файла
+    # data_files_path = 'C:\Users\EgorS\PycharmProjects\Automation_01\data_files'
+    data_1 = read_file(path='/home/user/first_name.txt')
+    data_2 = read_file(path='/home/user/last_name.txt')
+    print(data_1)
+    print(data_2)
 
     # MySQL #######################################################
     # Open database connection
